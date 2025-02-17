0e7c07d7ced10ae109ceca4e6daccc7298a5b8b8
NicoGartmann <nico.gartmann97+github@gmail.com>
2025-02-17 12:39:03 +0100

    aboutme section

--- src/app/Aboutme.tsx
+++ src/app/Aboutme.tsx
@@ -1,3 +1,6 @@
+import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+
 export default function Aboutme({
     id
 }: {
@@ -6,6 +9,41 @@ export default function Aboutme({
     return (
         <div>
             <h2>Über Mich</h2>
+            <p>Ich gehe nun seit mittlerweile 4 Jahren meiner Leidenschaft nach:</p>
+            <p>Professionelles Marketing</p>
+
+            <Card>
+                <CardContent>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    </div>
+                    <div>
+                        <Image 
+                            src="/platzhalter.png"
+                            alt="Picture of Nadja"
+                            width={200}
+                            height={200}
+                        />
+                    </div>
+                </CardContent>
+            </Card>
+
+            <Card>
+                <CardContent>
+                    <div>
+                        <Image 
+                            src="/platzhalter.png"
+                            alt="Picture of Nadja"
+                            width={200}
+                            height={200}
+                        />
+                    </div>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    </div>
+                </CardContent>
+            </Card>
+
         </div>
     )
 }
