64321881bfad5d9297dab8fe2c8239e6300900de
NicoGartmann <nico.gartmann97+github@gmail.com>
2025-02-17 12:38:54 +0100

    services section

--- src/app/Services.tsx
+++ src/app/Services.tsx
@@ -6,6 +6,19 @@ export default function Services({
     return (
         <div>
             <h2>Services</h2>
+
+            <h3>Marketingberatung</h3>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+        
+            <h3>Performance</h3>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+
+            <h3>Avatar Hacking</h3>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+
+            <h3>Marketingberatung</h3>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+        
         </div>
     )
 }
