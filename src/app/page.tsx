b03ac262f127c81ce60725939b33227d68f2504c
NicoGartmann <nico.gartmann97+github@gmail.com>
2025-02-17 09:50:15 +0100

    Added default class settings from nextjs template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Header from "./Header";
 import Footer from "./Footer";
 import Services from "./Services";
@@ -8,9 +7,9 @@ import Contact from "./Contact";
 
 export default function Home() {
   return (
-    <div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header/>
-      <main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Services id="services"/>
         <Aboutme id="aboutme"/>
         <Book id="book"/>
